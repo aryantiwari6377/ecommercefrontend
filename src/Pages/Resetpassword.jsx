ac2f98ed6842e18ddd70ab5499013decc9ba851c
aryan <aryansjpr2@gmail.com>
2025-04-23 17:44:01 +0530

    update reset file

--- src/Pages/Resetpassword.jsx
+++ src/Pages/Resetpassword.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
-
+import {resetpasswordService} from '../services/authenticationService';
 function Resetpassword() {
   const [newpassword, setnewpassword] = useState("");
   const { token } = useParams();
